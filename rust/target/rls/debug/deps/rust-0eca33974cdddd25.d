d0a6913ab94ad482f448c5b14ed71e635a5f2220
Administrator <11>
2022-01-08 21:24:43 +0900

    1st

---
+++ rust/target/rls/debug/deps/rust-0eca33974cdddd25.d
@@ -0,0 +1,5 @@
+c:\Users\Administrator\Documents\rust-go\rust\target\rls\debug\deps\rust-0eca33974cdddd25.rmeta: src\main.rs
+
+c:\Users\Administrator\Documents\rust-go\rust\target\rls\debug\deps\rust-0eca33974cdddd25.d: src\main.rs
+
+src\main.rs:
---
+++ rust/target/rls/debug/deps/rust-f41f8a44befcae5d.d
@@ -0,0 +1,5 @@
+c:\Users\Administrator\Documents\rust-go\rust\target\rls\debug\deps\rust-f41f8a44befcae5d.rmeta: src\main.rs
+
+c:\Users\Administrator\Documents\rust-go\rust\target\rls\debug\deps\rust-f41f8a44befcae5d.d: src\main.rs
+
+src\main.rs:
